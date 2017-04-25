2afbb879de765c2f369df507126eab23f0079742
Ilan Biala <ilan.biala@gmail.com>
2017-04-25 17:51:00 -0400

    tests found that simulator implementations for multiply() and divide() were switched

--- simulator.py
+++ simulator.py
@@ -2,10 +2,10 @@ def add(x, y):
     return x + y
 
 def multiply(x, y):
-    return x / y
+    return x * y
 
 def divide(x, y):
-    return x * y
+    return x / y
 
 def square(x):
     return x ** 2
