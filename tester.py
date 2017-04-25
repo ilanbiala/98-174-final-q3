64de6ef26e87f083d0958fb813f18a29b76a8931
Ilan Biala <ilan.biala@gmail.com>
2017-04-25 17:44:16 -0400

    Added tests for 4 function simulator calculator

--- tester.py
+++ tester.py
@@ -1,37 +1,68 @@
 def testAdd(x, y):
     print("Testing add()...", end="")
-
+    assert(add(0, 0) == 0)
+    assert(add(1, 2) == 3)
+    assert(add(-1, -2) == -3)
+    assert(add(-1, 2) == 1)
     print("passed!")
-    return x + y
 
 def testMultiply(x, y):
     print("Testing multiply()...", end="")
-
+    assert(multiply(0, 0) == 0)
+    assert(multiply(0, 1) == 0)
+    assert(multiply(0, -1) == 0)
+    assert(multiply(1, 1) == 1)
+    assert(multiply(1, -1) == -1)
+    assert(multiply(-1, -1) == 1)
+    assert(multiply(2, 4) == 8)
+    assert(multiply(2, -4) == -8)
+    assert(multiply(-2, 4) == -8)
+    assert(multiply(-2, -4) == 8)
     print("passed!")
     return x / y
 
 def testDivide(x, y):
     print("Testing divide()...", end="")
-
+    assert(divide(0, 1) == 0)
+    assert(divide(0, -1) == 0)
+    assert(divide(1, 1) == 1)
+    assert(divide(1, -1) == -1)
+    assert(divide(-1, -1) == 1)
+    assert(divide(2, 4) == 1/2)
+    assert(divide(2, -4) == -1/2)
+    assert(divide(-2, 4) == -1/2)
+    assert(divide(-2, -4) == 1/2)
+    assert(divide(4, 2) == 2)
+    assert(divide(-4, 2) == -2)
+    assert(divide(4, -2) == -2)
+    assert(divide(-4, -2) == 2)
     print("passed!")
     return x * y
 
 def testSquare(x):
     print("Testing square()...", end="")
-
+    assert(square(0) == 0)
+    assert(square(1) == 1)
+    assert(square(-1) == -1)
+    assert(square(--1) == 1)
+    assert(square(2) == 4)
+    assert(square(2/4) == 1/4)
+    assert(square(4) == 16)
+    assert(square(-4) == 16)
+    assert(square(4/2) == 4)
+    assert(square(4/2) == square(2))
     print("passed!")
     return x ** 2
 
 def testMod(x, y):
     print("Testing mod()...", end="")
-
-    print("passed!")
+    raise Exception("Will be added later!")
     return x % y
 
 def testDistance(x1, y1, x2, y2):
     print("Testing distance()...", end="")
 
-    print("passed!")
+    raise Exception("Will be added later!")
     return sqrt(square(x2 - x1, x2 - x1) + square(y2 - x1, y2 - x1))
 
 def testAll():
